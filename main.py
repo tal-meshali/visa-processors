836f7fb3e2cad3433194687ef755023af3556533
Tal Meshali <meshalital@gmail.com>
2026-01-31 21:42:16 +0200

    Adding main file for cloud functions

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+
+import functions_framework
+from flask import Request
+from google.cloud import firestore
+
+from controllers.controllers import MoroccoController, TanzaniaController
+
+
+def get_firestore_client() -> firestore.Client:
+    """Initialize and return Firestore client"""
+    project_id = os.getenv("GCP_PROJECT_ID")
+    database_id = os.getenv("FIRESTORE_DATABASE")
+
+    return firestore.Client(
+        project=project_id,
+        database=database_id
+    )
+
+
+_FIRESTORE = get_firestore_client()
+_BUCKET_NAME = os.getenv("GCS_BUCKET_NAME")
+
+country_to_controller = {
+    "morocco": MoroccoController,
+    "tanzania": TanzaniaController
+}
+
+
+@functions_framework.http
+async def process_morocco(request: Request):
+    controller = country_to_controller[request.json['country']](firestore_client=_FIRESTORE, bucket_name=_BUCKET_NAME)
+    return controller.process_request(request.json['request_id']).as_dict()
